d9646422b7b74de232ac9d6b3d2f37e7c090ee79
Majid-Ali01 <go7015561@gmail.com>
2026-02-13 00:08:47 -0800

    database week two work

---
+++ DBMS2.sql
@@ -0,0 +1,245 @@
+create schema sectioa;
+create database student;
+CREATE TABLE sectioa.studrent (
+    cms INT PRIMARY KEY,
+    name VARCHAR(50),
+    country VARCHAR(50),
+    age INT,
+    score INT
+);
+
+INSERT INTO sectioa.studrent (cms ,   name ,   country ,   age ,   score) VALUES
+(1 ,  'Student1' ,  'Pakistan' ,  18 ,  51) ,  
+(2 ,  'Student2' ,  'India' ,  19 ,  52) ,  
+(3 ,  'Student3' ,  'Turkey' ,  20 ,  53) ,  
+(4 ,  'Student4' ,  'Italy' ,  21 ,  54) ,  
+(5 ,  'Student5' ,  'Germany' ,  22 ,  55) ,  
+(6 ,  'Student6' ,  'France' ,  23 ,  56) ,  
+(7 ,  'Student7' ,  'UAE' ,  24 ,  57) ,  
+(8 ,  'Student8' ,  'USA' ,  25 ,  58) ,  
+(9 ,  'Student9' ,  'Canada' ,  26 ,  59) ,  
+(10 ,  'Student10' ,  'Australia' ,  27 ,  60) ,  
+(11 ,  'Student11' ,  'Pakistan' ,  18 ,  61) ,  
+(12 ,  'Student12' ,  'India' ,  19 ,  62) ,  
+(13 ,  'Student13' ,  'Turkey' ,  20 ,  63) ,  
+(14 ,  'Student14' ,  'Italy' ,  21 ,  64) ,  
+(15 ,  'Student15' ,  'Germany' ,  22 ,  65) ,  
+(16 ,  'Student16' ,  'France' ,  23 ,  66) ,  
+(17 ,  'Student17' ,  'UAE' ,  24 ,  67) ,  
+(18 ,  'Student18' ,  'USA' ,  25 ,  68) ,  
+(19 ,  'Student19' ,  'Canada' ,  26 ,  69) ,  
+(20 ,  'Student20' ,  'Australia' ,  27 ,  70) ,  
+(21 ,  'Student21' ,  'Pakistan' ,  18 ,  71) ,  
+(22 ,  'Student22' ,  'India' ,  19 ,  72) ,  
+(23 ,  'Student23' ,  'Turkey' ,  20 ,  73) ,  
+(24 ,  'Student24' ,  'Italy' ,  21 ,  74) ,  
+(25 ,  'Student25' ,  'Germany' ,  22 ,  75) ,  
+(26 ,  'Student26' ,  'France' ,  23 ,  76) ,  
+(27 ,  'Student27' ,  'UAE' ,  24 ,  77) ,  
+(28 ,  'Student28' ,  'USA' ,  25 ,  78) ,  
+(29 ,  'Student29' ,  'Canada' ,  26 ,  79) ,  
+(30 ,  'Student30' ,  'Australia' ,  27 ,  80) ,  
+(31 ,  'Student31' ,  'Pakistan' ,  18 ,  81) ,  
+(32 ,  'Student32' ,  'India' ,  19 ,  82) ,  
+(33 ,  'Student33' ,  'Turkey' ,  20 ,  83) ,  
+(34 ,  'Student34' ,  'Italy' ,  21 ,  84) ,  
+(35 ,  'Student35' ,  'Germany' ,  22 ,  85) ,  
+(36 ,  'Student36' ,  'France' ,  23 ,  86) ,  
+(37 ,  'Student37' ,  'UAE' ,  24 ,  87) ,  
+(38 ,  'Student38' ,  'USA' ,  25 ,  88) ,  
+(39 ,  'Student39' ,  'Canada' ,  26 ,  89) ,  
+(40 ,  'Student40' ,  'Australia' ,  27 ,  90) ,  
+(41 ,  'Student41' ,  'Pakistan' ,  18 ,  91) ,  
+(42 ,  'Student42' ,  'India' ,  19 ,  92) ,  
+(43 ,  'Student43' ,  'Turkey' ,  20 ,  93) ,  
+(44 ,  'Student44' ,  'Italy' ,  21 ,  94) ,  
+(45 ,  'Student45' ,  'Germany' ,  22 ,  95) ,  
+(46 ,  'Student46' ,  'France' ,  23 ,  96) ,  
+(47 ,  'Student47' ,  'UAE' ,  24 ,  97) ,  
+(48 ,  'Student48' ,  'USA' ,  25 ,  98) ,  
+(49 ,  'Student49' ,  'Canada' ,  26 ,  99) ,  
+(50 ,  'Student50' ,  'Australia' ,  27 ,  100) ,  
+(51 ,  'Student51' ,  'Pakistan' ,  18 ,  55) ,  
+(52 ,  'Student52' ,  'India' ,  19 ,  56) ,  
+(53 ,  'Student53' ,  'Turkey' ,  20 ,  57) ,  
+(54 ,  'Student54' ,  'Italy' ,  21 ,  58) ,  
+(55 ,  'Student55' ,  'Germany' ,  22 ,  59) ,  
+(56 ,  'Student56' ,  'France' ,  23 ,  60) ,  
+(57 ,  'Student57' ,  'UAE' ,  24 ,  61) ,  
+(58 ,  'Student58' ,  'USA' ,  25 ,  62) ,  
+(59 ,  'Student59' ,  'Canada' ,  26 ,  63) ,  
+(60 ,  'Student60' ,  'Australia' ,  27 ,  64) ,  
+(61 ,  'Student61' ,  'Pakistan' ,  18 ,  65) ,  
+(62 ,  'Student62' ,  'India' ,  19 ,  66) ,  
+(63 ,  'Student63' ,  'Turkey' ,  20 ,  67) ,  
+(64 ,  'Student64' ,  'Italy' ,  21 ,  68) ,  
+(65 ,  'Student65' ,  'Germany' ,  22 ,  69) ,  
+(66 ,  'Student66' ,  'France' ,  23 ,  70) ,  
+(67 ,  'Student67' ,  'UAE' ,  24 ,  71) ,  
+(68 ,  'Student68' ,  'USA' ,  25 ,  72) ,  
+(69 ,  'Student69' ,  'Canada' ,  26 ,  73) ,  
+(70 ,  'Student70' ,  'Australia' ,  27 ,  74) ,  
+(71 ,  'Student71' ,  'Pakistan' ,  18 ,  75) ,  
+(72 ,  'Student72' ,  'India' ,  19 ,  76) ,  
+(73 ,  'Student73' ,  'Turkey' ,  20 ,  77) ,  
+(74 ,  'Student74' ,  'Italy' ,  21 ,  78) ,  
+(75 ,  'Student75' ,  'Germany' ,  22 ,  79) ,  
+(76 ,  'Student76' ,  'France' ,  23 ,  80) ,  
+(77 ,  'Student77' ,  'UAE' ,  24 ,  81) ,  
+(78 ,  'Student78' ,  'USA' ,  25 ,  82) ,  
+(79 ,  'Student79' ,  'Canada' ,  26 ,  83) ,  
+(80 ,  'Student80' ,  'Australia' ,  27 ,  84) ,  
+(81 ,  'Student81' ,  'Pakistan' ,  18 ,  85) ,  
+(82 ,  'Student82' ,  'India' ,  19 ,  86) ,  
+(83 ,  'Student83' ,  'Turkey' ,  20 ,  87) ,  
+(84 ,  'Student84' ,  'Italy' ,  21 ,  88) ,  
+(85 ,  'Student85' ,  'Germany' ,  22 ,  89) ,  
+(86 ,  'Student86' ,  'France' ,  23 ,  90) ,  
+(87 ,  'Student87' ,  'UAE' ,  24 ,  91) ,  
+(88 ,  'Student88' ,  'USA' ,  25 ,  92) ,  
+(89 ,  'Student89' ,  'Canada' ,  26 ,  93) ;  
+SELECT * FROM sectioa.studrent;
+UPDATE sectioa.studrent SET name='Ali Ahmed' WHERE cms=1;
+UPDATE sectioa.studrent SET name='Hyder Ali' WHERE cms=2;
+UPDATE sectioa.studrent SET name='Ahmed Raza' WHERE cms=3;
+UPDATE sectioa.studrent SET name='Usman Khan' WHERE cms=4;
+UPDATE sectioa.studrent SET name='Bilal Hussain' WHERE cms=5;
+UPDATE sectioa.studrent SET name='Hamza Tariq' WHERE cms=6;
+UPDATE sectioa.studrent SET name='Saad Malik' WHERE cms=7;
+UPDATE sectioa.studrent SET name='Zain Abbas' WHERE cms=8;
+UPDATE sectioa.studrent SET name='Farhan Ali' WHERE cms=9;
+UPDATE sectioa.studrent SET name='Talha Noor' WHERE cms=10;
+UPDATE sectioa.studrent SET name='Hassan Raza' WHERE cms=11;
+UPDATE sectioa.studrent SET name='Danish Khan' WHERE cms=12;
+UPDATE sectioa.studrent SET name='Owais Ahmed' WHERE cms=13;
+UPDATE sectioa.studrent SET name='Shahzaib Ali' WHERE cms=14;
+UPDATE sectioa.studrent SET name='Awais Malik' WHERE cms=15;
+UPDATE sectioa.studrent SET name='Imran Hussain' WHERE cms=16;
+UPDATE sectioa.studrent SET name='Adnan Sheikh' WHERE cms=17;
+UPDATE sectioa.studrent SET name='Kamran Ali' WHERE cms=18;
+UPDATE sectioa.studrent SET name='Noman Iqbal' WHERE cms=19;
+UPDATE sectioa.studrent SET name='Sufyan Ahmed' WHERE cms=20;
+UPDATE sectioa.studrent SET name='Rizwan Ali' WHERE cms=21;
+UPDATE sectioa.studrent SET name='Yasir Khan' WHERE cms=22;
+UPDATE sectioa.studrent SET name='Mudassir Raza' WHERE cms=23;
+UPDATE sectioa.studrent SET name='Junaid Malik' WHERE cms=24;
+UPDATE sectioa.studrent SET name='Fahad Ali' WHERE cms=25;
+UPDATE sectioa.studrent SET name='Kashif Hussain' WHERE cms=26;
+UPDATE sectioa.studrent SET name='Atif Raza' WHERE cms=27;
+UPDATE sectioa.studrent SET name='Waqar Ahmed' WHERE cms=28;
+UPDATE sectioa.studrent SET name='Shahbaz Khan' WHERE cms=29;
+UPDATE sectioa.studrent SET name='Arslan Ali' WHERE cms=30;
+UPDATE sectioa.studrent SET name='Rehan Malik' WHERE cms=31;
+UPDATE sectioa.studrent SET name='Adeel Ahmed' WHERE cms=32;
+UPDATE sectioa.studrent SET name='Salman Raza' WHERE cms=33;
+UPDATE sectioa.studrent SET name='Asad Khan' WHERE cms=34;
+UPDATE sectioa.studrent SET name='Zubair Ali' WHERE cms=35;
+UPDATE sectioa.studrent SET name='Irfan Malik' WHERE cms=36;
+UPDATE sectioa.studrent SET name='Majid Hussain' WHERE cms=37;
+UPDATE sectioa.studrent SET name='Tariq Mehmood' WHERE cms=38;
+UPDATE sectioa.studrent SET name='Naveed Ahmed' WHERE cms=39;
+UPDATE sectioa.studrent SET name='Sajid Ali' WHERE cms=40;
+UPDATE sectioa.studrent SET name='Haroon Raza' WHERE cms=41;
+UPDATE sectioa.studrent SET name='Qasim Khan' WHERE cms=42;
+UPDATE sectioa.studrent SET name='Umer Farooq' WHERE cms=43;
+UPDATE sectioa.studrent SET name='Shahid Malik' WHERE cms=44;
+UPDATE sectioa.studrent SET name='Waqas Ahmed' WHERE cms=45;
+UPDATE sectioa.studrent SET name='Rashid Ali' WHERE cms=46;
+UPDATE sectioa.studrent SET name='Naeem Khan' WHERE cms=47;
+UPDATE sectioa.studrent SET name='Aamir Hussain' WHERE cms=48;
+UPDATE sectioa.studrent SET name='Basit Ali' WHERE cms=49;
+UPDATE sectioa.studrent SET name='Haris Raza' WHERE cms=50;
+UPDATE sectioa.studrent SET name='Imtiaz Ahmed' WHERE cms=51;
+UPDATE sectioa.studrent SET name='Zeeshan Ali' WHERE cms=52;
+UPDATE sectioa.studrent SET name='Faisal Khan' WHERE cms=53;
+UPDATE sectioa.studrent SET name='Muneeb Raza' WHERE cms=54;
+UPDATE sectioa.studrent SET name='Rameez Malik' WHERE cms=55;
+UPDATE sectioa.studrent SET name='Taha Ahmed' WHERE cms=56;
+UPDATE sectioa.studrent SET name='Sameer Ali' WHERE cms=57;
+UPDATE sectioa.studrent SET name='Yahya Khan' WHERE cms=58;
+UPDATE sectioa.studrent SET name='Shayan Raza' WHERE cms=59;
+UPDATE sectioa.studrent SET name='Dawood Malik' WHERE cms=60;
+UPDATE sectioa.studrent SET name='Haider Abbas' WHERE cms=61;
+UPDATE sectioa.studrent SET name='Azlan Khan' WHERE cms=62;
+UPDATE sectioa.studrent SET name='Huzaifa Ahmed' WHERE cms=63;
+UPDATE sectioa.studrent SET name='Ahsan Raza' WHERE cms=64;
+UPDATE sectioa.studrent SET name='Subhan Ali' WHERE cms=65;
+UPDATE sectioa.studrent SET name='Mustafa Khan' WHERE cms=66;
+UPDATE sectioa.studrent SET name='Rayyan Ahmed' WHERE cms=67;
+UPDATE sectioa.studrent SET name='Hammad Raza' WHERE cms=68;
+UPDATE sectioa.studrent SET name='Shariq Malik' WHERE cms=69;
+UPDATE sectioa.studrent SET name='Umair Khan' WHERE cms=70;
+UPDATE sectioa.studrent SET name='Faizan Ali' WHERE cms=71;
+UPDATE sectioa.studrent SET name='Jawad Ahmed' WHERE cms=72;
+UPDATE sectioa.studrent SET name='Mehroz Khan' WHERE cms=73;
+UPDATE sectioa.studrent SET name='Saif Raza' WHERE cms=74;
+UPDATE sectioa.studrent SET name='Rayan Malik' WHERE cms=75;
+UPDATE sectioa.studrent SET name='Talal Ahmed' WHERE cms=76;
+UPDATE sectioa.studrent SET name='Samiullah Khan' WHERE cms=77;
+UPDATE sectioa.studrent SET name='Hanzala Raza' WHERE cms=78;
+UPDATE sectioa.studrent SET name='Arham Ali' WHERE cms=79;
+UPDATE sectioa.studrent SET name='Shayan Ahmed' WHERE cms=80;
+UPDATE sectioa.studrent SET name='Areeb Khan' WHERE cms=81;
+UPDATE sectioa.studrent SET name='Muzammil Raza' WHERE cms=82;
+UPDATE sectioa.studrent SET name='Sarim Ali' WHERE cms=83;
+UPDATE sectioa.studrent SET name='Hasnain Ahmed' WHERE cms=84;
+UPDATE sectioa.studrent SET name='Zarar Khan' WHERE cms=85;
+UPDATE sectioa.studrent SET name='Afaq Raza' WHERE cms=86;
+UPDATE sectioa.studrent SET name='Kamil Ahmed' WHERE cms=87;
+UPDATE sectioa.studrent SET name='Taimoor Khan' WHERE cms=88;
+UPDATE sectioa.studrent SET name='Salar Raza' WHERE cms=89;
+UPDATE sectioa.studrent SET name='Zainul Abideen' WHERE cms=90;
+UPDATE sectioa.studrent SET name='Ashar Khan' WHERE cms=91;
+UPDATE sectioa.studrent SET name='Ayaan Raza' WHERE cms=92;
+UPDATE sectioa.studrent SET name='Rafay Ahmed' WHERE cms=93;
+UPDATE sectioa.studrent SET name='Hashir Khan' WHERE cms=94;
+UPDATE sectioa.studrent SET name='Shams Ali' WHERE cms=95;
+UPDATE sectioa.studrent SET name='Adil Hussain' WHERE cms=96;
+UPDATE sectioa.studrent SET name='Haseeb Raza' WHERE cms=97;
+UPDATE sectioa.studrent SET name='Furqan Ahmed' WHERE cms=98;
+UPDATE sectioa.studrent SET name='Anas Khan' WHERE cms=99;
+UPDATE sectioa.studrent SET name='Nashit Raza' WHERE cms=100;
+SELECT * FROM sectioa.studrent;
+select age, count(age) from sectioa.studrent
+group by age;
+SELECT age,
+       COUNT(age) AS occurance,
+       SUM(score) AS total_score
+FROM sectioa.studrent
+GROUP BY age;
+SELECT 
+    country,
+    age,
+    COUNT(age) AS occurance,
+    SUM(score) AS total_score
+FROM sectioa.studrent
+GROUP BY country, age
+ORDER BY country, age;
+
+SELECT 
+    country,
+    COUNT(*) AS occurance,
+    SUM(score) AS total_score,
+    AVG(score) AS average_score,
+    MIN(score) AS minimum_score,
+    MAX(score) AS maximum_score
+FROM sectioa.studrent
+GROUP BY country
+ORDER BY country;
+select * from  sectioa.studrent where name like 'W%' ;
+select * from  sectioa.studrent where name like 'd%' ;
+select * from  sectioa.studrent where name like '%d' ;
+select * from  sectioa.studrent where score like '_1' ;
+select * from  sectioa.studrent where score like '_4' ;
+select * from  sectioa.studrent where score like '_7' ;
+select * from  sectioa.studrent where score like '4_' ;
+select * from  sectioa.studrent where name like '%a_' ;
+select * from  sectioa.studrent where score=48 or score=38 or score=40;
+select * from  sectioa.studrent where score between 60  AND 70 ;
+
+
+
+
+
+
+
